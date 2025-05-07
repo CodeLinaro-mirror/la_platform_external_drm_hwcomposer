094b09d976e88621d574e72b6440bd90f3c7285e
Sasha McIntosh <sashamcintosh@google.com>
2025-05-07 15:39:18 -0400

    drm_hwcomposer: Use latest available composer3 API
    This will allow us to cherry-pick downstream changes from Android16
    without breaking those using the Android15 source tree.
    
    Add Android15 target to CXXFLAGS so that Android16 APIs are
    ignored on CI.
    
    Add ANDROID_SDK_VERSION to ci builds.
    
    Change-Id: I4e37e5ce5b2c2b5dc5134dc8c33f284fe26d4019
    Signed-off-by: Sasha McIntosh <sashamcintosh@google.com>

--- .ci/container/ubuntu/x86_64_aospless.sh
+++ .ci/container/ubuntu/x86_64_aospless.sh
@@ -86,6 +86,7 @@ rm external/libdisplay_info -rf
 git clone --depth=1 https://android.googlesource.com/platform/external/libdisplay-info/ external/libdisplay_info
 
 git clone https://github.com/GloDroid/aospext.git
+sed -i "s|cpp_args = \['\[CPP_ARGS\]'\]|cpp_args = ['[CPP_ARGS]', '-D__ANDROID_API__=${ANDROID_SDK_VERSION}']|" "${TOP}/aospext/tools/meson_aosp_cross.cfg"
 cat >> "${TOP}/device/google/cuttlefish/shared/device.mk" <<EOF
 BOARD_BUILD_AOSPEXT_DRMHWCOMPOSER := true
 BOARD_DRMHWCOMPOSER_SRC_DIR := external/drm_hwcomposer
