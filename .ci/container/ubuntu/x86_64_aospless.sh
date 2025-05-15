851738b8cb5df63e1860bbb2ef554bf53fe02a3c
Deborah Brouwer <deborah.brouwer@collabora.com>
2025-05-15 11:20:28 -0700

    drm_hwcomposer: CI: use dev fork to build container
    If a contributor is a member of an upstream project, then the built-in
    GitLab variable CI_PROJECT_URL points to the upstream project. This
    means that a developer's fork in a merge request can't be used to build
    a container.
    
    Replace CI_PROJECT_URL and point to the developer's fork repository
    instead.
    
    Signed-off-by: Deborah Brouwer <deborah.brouwer@collabora.com>

--- .ci/container/ubuntu/x86_64_aospless.sh
+++ .ci/container/ubuntu/x86_64_aospless.sh
@@ -73,8 +73,14 @@ yes n | repo init \
 time repo sync --fail-fast --no-tags -j2
 
 rm external/drm_hwcomposer -rf
-git clone --depth=1 --branch "${CI_COMMIT_REF_NAME}" "${CI_PROJECT_URL}" external/drm_hwcomposer
-git -C external/drm_hwcomposer checkout "${CI_COMMIT_SHA}"
+
+git clone "${CI_REPOSITORY_URL}" external/drm_hwcomposer
+if [[ "${CI_PIPELINE_SOURCE}" == "merge_request_event" ]]; then
+  git -C external/drm_hwcomposer fetch origin "${CI_MERGE_REQUEST_REF_PATH}"
+else
+  git -C external/drm_hwcomposer fetch origin "${CI_COMMIT_REF_NAME}"
+fi
+git -C external/drm_hwcomposer checkout FETCH_HEAD
 
 rm external/libdisplay_info -rf
 git clone --depth=1 https://android.googlesource.com/platform/external/libdisplay-info/ external/libdisplay_info
