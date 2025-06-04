ac63f967e82995565a528d95c21e05ac921f522b
Deborah Brouwer <deborah.brouwer@collabora.com>
2025-06-04 16:36:42 -0700

    drm_hwcomposer: CI: Update cuttlefish packages
    Since the android container is being rebuilt, update the cuttlefish
    packages that it uses from v1.0.1 -> v.1.5.0.
    
    Signed-off-by: Deborah Brouwer <deborah.brouwer@collabora.com>

--- .ci/container/ubuntu/x86_64_android.sh
+++ .ci/container/ubuntu/x86_64_android.sh
@@ -39,7 +39,7 @@ section_end install_packages
 # Build and install Debian package for cuttlefish
 section_start get_cuttlefish_packages "get_cuttlefish_packages"
 set -x
-ANDROID_CUTTLEFISH_VERSION=v1.0.1
+ANDROID_CUTTLEFISH_VERSION=v1.5.0
 mkdir /android-cuttlefish
 pushd /android-cuttlefish
 git init
@@ -113,4 +113,5 @@ set +x
 section_end build_vts
 
 # clean up
+rm "/root/.cache" -rf
 rm "${TOP}" -rf
