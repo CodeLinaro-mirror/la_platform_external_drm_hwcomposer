9e5c37ae088d2de80368aac38dd46864808ea034
Deborah Brouwer <deborah.brouwer@collabora.com>
2025-06-11 17:05:46 -0700

    drm_hwcomposer: CI: Use new formatting for android
    Replace the custom formatting functions from setup-test-env.sh with the
    new formatting functions available from ci-templates.
    
    Signed-off-by: Deborah Brouwer <deborah.brouwer@collabora.com>

--- .ci/android/vts-runner.sh
+++ .ci/android/vts-runner.sh
@@ -11,8 +11,7 @@ fi
 
 source "${CI_PROJECT_DIR}/.ci/android/launch-cvd.sh"
 
-section_start run_android_vts "run_android_vts"
-set -x
+fdo_log_section_start_collapsed run_android_vts "run_android_vts"
 adb wait-for-device devices
 adb root
 adb push /VtsHalGraphicsComposer3_TargetTest /data/local/tmp/
@@ -30,4 +29,4 @@ adb pull /data/local/tmp/vts_results.txt "${RESULTS_DIR}"
 ! grep "FAILED" "${RESULTS_DIR}/vts_results.txt"
 export EXIT_CODE=$?
 
-section_end run_android_vts
+fdo_log_section_end run_android_vts
