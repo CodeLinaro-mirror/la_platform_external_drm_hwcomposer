1a8faf12a7266b98b3dfc8ba8b41e38550d59a6a
Sean Paul <seanpaul@google.com>
2026-05-14 16:02:21 +0000

    Revert "Set the default HDR headroom"
    This reverts commit e9b6e46670a1ef71a4c037dd9c25c5c46fb391bb.

--- hwc/HwcDisplay.h
+++ hwc/HwcDisplay.h
@@ -358,7 +358,6 @@ class HwcDisplay : public ICompositorDisplay {
 
   bool Init();
 
-  void SetHdrHeadroom();
   void SetHdrOutputMetadata(ui::Hdr hdrType);
   void SetOutputType(OutputType hdr_output_type);
 
@@ -404,7 +403,6 @@ class HwcDisplay : public ICompositorDisplay {
   int32_t min_bpc_{};
   std::shared_ptr<hdr_output_metadata> hdr_metadata_;
   float brightness_ = -1.F;
-  float hdr_headroom_{};
 
   // Most recent result of ValidateStagedComposition. Must be kept alive until
   // the composition is committed.
