22210f20dda9c2b4660cc2497b4c95237c56445a
Su Hong Koo <sukoo@google.com>
2026-07-20 15:02:08 -0400

    Add option to client composite cursor for CTM offset
    Add an optional flow in GenericLayerMapperCompositionPlanner to
    client composite the cursor layer if the CTM has an offset.
    
    Test: Manually verified that the cursor is client composited w/ offset
    Change-Id: I51ee9c93647bf2fc5adc213292691e2f98e8a4ba

--- compositor/GenericLayerMapperCompositionPlanner.cpp
+++ compositor/GenericLayerMapperCompositionPlanner.cpp
@@ -16,12 +16,16 @@
 #include "GenericLayerMapperCompositionPlanner.h"
 
 #include <algorithm>
+#include <cmath>
 #include <cstddef>
+#include <limits>
+#include <memory>
 #include <optional>
 #include <utility>
 #include <vector>
 
 #include "compositor/CompositionPlanner.h"
+#include "compositor/DisplayInfo.h"
 #include "compositor/FlatteningController.h"
 #include "compositor/LayerData.h"
 #include "compositor/ShortCircuitor.h"
@@ -29,6 +33,7 @@
 #include "compositor/mapper/MapperUtils.h"
 #include "drm/CommitStatus.h"
 #include "hwc/HwcLayer.h"
+#include "utils/properties.h"
 
 namespace android::drm_hwcomposer {
 
@@ -161,6 +166,23 @@ CommitStatus TestLayerMappings(
   }
 
   return result;
+}
+
+bool HasOffset(const std::shared_ptr<const HalColorTransforMatrix>& matrix) {
+  if (!matrix) {
+    return false;
+  }
+
+  constexpr int kOffsetStart = 12;
+  constexpr int kOffsetEnd = 14;
+  for (int i = kOffsetStart; i < kOffsetEnd; i++) {
+    constexpr float kEpsilon = std::numeric_limits<float>::epsilon();
+    if (std::abs(matrix->at(i) - 0.F) > kEpsilon) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 }  // namespace
@@ -300,6 +322,11 @@ GenericLayerMapperCompositionPlanner::CreateFlattenedComposition(
 CompositionType GenericLayerMapperCompositionPlanner::GetCursorCompositionType(
     const ICompositorDisplay* display,
     const std::vector<LayerMapping>& layers) const {
+  if (Properties::BugfixCursorCtmOffset() &&
+      HasOffset(display->GetColorTransformMatrix())) {
+    return CompositionType::kClient;
+  }
+
   if (DisplayCanUseCursorPlane(display, GetCursorLayer(layers))) {
     // Create and test a composition using only cursor plane and all other
     // layers client-composited to infer whether the cursor plane can be used.
